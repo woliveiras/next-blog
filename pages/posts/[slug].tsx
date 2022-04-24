c628d61f3c51fe3076af777f49f397fb851f7974
William Oliveira <william.souza@quintoandar.com.br>
2022-04-24 17:24:20 -0300

    feat(Posts): add highlighter

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -2,6 +2,8 @@ import fs from "fs";
 import matter from "gray-matter";
 import rehypeRaw from 'rehype-raw'
 import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 import { GetServerSideProps } from "next";
 
@@ -14,7 +16,28 @@ export default function PostTemplate({ frontmatter, content } : {
   return (
     <div className="prose mx-auto">
       <h1>{frontmatter.title}</h1>
-      <ReactMarkdown rehypePlugins={[rehypeRaw]}>{content}</ReactMarkdown>
+      <ReactMarkdown 
+        rehypePlugins={[rehypeRaw]}
+        components={{
+            code({node, inline, className, children, ...props}) {
+              const match = /language-(\w+)/.exec(className || '')
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  style={vscDarkPlus}
+                  language={match[1]}
+                  PreTag="div"
+                  {...props}
+                >
+                    {String(children).replace(/\n$/, '')}
+                </SyntaxHighlighter>
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              )
+            }
+          }}
+        >{content}</ReactMarkdown>
     </div>
   );
 }
